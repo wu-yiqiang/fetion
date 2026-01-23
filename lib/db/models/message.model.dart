b056baa24eae2a85212818641aed8fcb719c3fd0
wu_yiqiang <sutter.wu@itforce-tech.com>
2026-01-23 14:48:02 +0800

    添加消息表字段

---
+++ lib/db/models/message.model.dart
@@ -0,0 +1,14 @@
+import 'package:realm/realm.dart';
+part 'message.model.realm.dart';
+
+@RealmModel()
+class _Message {
+  @PrimaryKey()
+  late String id;
+  late String userId;
+  late bool isDeleted;
+  late String content;
+  late int msgType; // 0: 文本消息 1: 图片 3:文件
+  late int status; // 0:发送中 1:已送达 2:已读 3:测回
+  late String createTime;
+}
