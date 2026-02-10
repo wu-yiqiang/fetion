0db0697e3a4b4f2a0e6358b923dcc670e80283ca
wu_yiqiang <sutter.wu@itforce-tech.com>
2026-02-10 16:31:06 +0800

    删除多余引用

--- lib/widgets/Avatar.dart
+++ lib/widgets/Avatar.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:fluent_ui/fluent_ui.dart' hide Colors;
 import 'package:flutter/material.dart';
-import 'package:fetion/widgets/FluentIcon.dart';
 
 Avatar({double size = 40, String? image}) {
   final hasImage = image != null && image!.isNotEmpty;
